b355d9eb6ddfbfa00bc1d4ad20ca36ac03daa779
Dylan Pirade Ponglabba <dylan.pirade@ui.ac.id>
2025-11-30 05:52:09 +0700

    return to original

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from authentication.views import login_flutter, register_flutter, logout_flutter
+from authentication.views import login, register, logout
 
 app_name = 'authentication'
 
 urlpatterns = [
-    path('login/', login_flutter, name='login_flutter'),
-    path('register/', register_flutter, name='register_flutter'),
-    path('logout/', logout_flutter, name='logout_flutter'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout, name='logout'),
 ]
